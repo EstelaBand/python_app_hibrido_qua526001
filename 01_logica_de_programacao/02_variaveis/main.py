32544d92f7ac04287f6afb43c0bdd705e0f21d68
EstelaBand <estelacabral@gmail.com>
2025-10-17 16:41:08 -0300

    Meu primeiro commit. Aula do dia 17/10/2025.

---
+++ 01_logica_de_programacao/02_variaveis/main.py
@@ -0,0 +1,11 @@
+# declaração de variáveis
+nome_completo = "Estela Bandeira" # string
+idade = 50 # int
+altura = 1.72 # float
+programador = True # bool (boolean)
+
+# saída de dados
+print(nome_completo)
+print(idade)
+print(altura)
+print(programador)
---
+++ 01_logica_de_programacao/03_tipos_de_dados/main.py
@@ -0,0 +1,11 @@
+# declaraçao de variáveis
+nome = "Estela Bandeira"
+idade = 50
+altura = 1.72
+programador = True
+
+# exibe os tipos de dados
+print(type(nome))
+print(type(idade))
+print(type(altura))
+print(type(programador))
