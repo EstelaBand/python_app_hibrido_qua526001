a156baa33cd70322f76051391e171c68cdf983cc
EstelaBand <estelacabral@gmail.com>
2025-10-28 17:40:12 -0300

    Aula do dia 28/10/2025

---
+++ 00_atividades/Atividade_05/main.py
@@ -0,0 +1,22 @@
+# TODO: atividade
+
+# declaração de lista
+nomes = ["Mato Grosso do Sul", "Paraná", "Alagoas", "Bahia", "Ceará", "Maranhão", "Rio Grande do Sul", "Distrito Federal", "Goiás", "Mato Grosso", "Minas Gerais", "Rio de Janeiro", "São Paulo", "Roraima", "Santa Catarina", "Tocantis", "Espírito Santo", "Amapá", "Amazonas", "Pará", "Rondônia", "Rio Grande do Norte", "Sergipe", "Acre", "Paraíba", "Pernambuco", "Piauí"]
+
+# Exibe toda a lista
+for nome in nomes:
+    print(nome)
+
+# Ordena a lista em ordem alfabética
+nomes.sort()
+
+# Re-exibe a lista em ordem alfabética
+print("\nOrdem Alfabética:\n")
+for nome in nomes:
+    print(nome)
+
+
+
+"""
+Crie uma lista com os nomes de todos os estados brasileiros, e mostre na tela os estados em ordem alfabética.
+"""
