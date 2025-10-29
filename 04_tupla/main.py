d8512e0a4e2b1ca37e1e4d75ee1fa42de6aeb7a8
EstelaBand <estelacabral@gmail.com>
2025-10-29 16:57:39 -0300

    Aula do dia 29/10/2025

---
+++ 04_tupla/main.py
@@ -0,0 +1,14 @@
+# declaração de tupla
+dias_semana = (
+    "Domingo",
+    "Segunda-feira",
+    "Terça-feira",
+    "Quarta-feira",
+    "Quinta-feira",
+    "Sexta=feira",
+    "Sábado"
+)
+
+# saída de dados
+for dia in dias_semana:
+    print(dia)
